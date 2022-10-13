193f4cd6072286d2df4525e8537f95a605f5a506
patinocondee <patinocondee@educ.college-montreal.qc.ca>
2022-10-13 09:20:23 -0400

    commit initial

--- TP2.py
+++ TP2.py
@@ -5,23 +5,30 @@ import random
 # Le code choisit un chiffre a l'aleatoire
 number = random.randint(1, 99)
 print(number)
+nb_essais = 0
 # Fonction pour inscrire votre première estimation
 guess = int(input(" Inscrit un nombre entre 1 et 99: "))
-def nb_essais = otal = 0
-     entry = raw_input("Enter a value, or q to quit: ")
-     while entry != "q":
-         total += int(entry)
-         entry = raw_input("Enter a value, or q to quit: ")
-     print "The total is", total
 
 
-while number != guess:
+
+
+while True :
+    while number != guess:
      if guess < number: # Si votre estimation est inferieur au chiffre choisi,
+        nb_essais = nb_essais + 1
         print(" le chiffre rentré est plus petit que le chiffre choisi")
         guess = int(input("Inscrit un nombre entre 1 et 99: "))
      elif guess > number:
+        nb_essais = nb_essais + 1
         print(" le chiffre rentré est plus grand que le chiffre choisi")
         guess = int(input("Inscrit un nombre entre 1 et 99: "))
+     else:
+         nb_essais = nb_essais + 1
+         print("Tu l'as devine")
+         print ( f" Ca a pris {nb_essais} essais")
 
-print("Tu l'as devine")
-print (nb_essais)
+         play_again = str(input("Voulez-vous jouer encore? y/n ;"))
+         if play_again == ("y") :
+             continue
+         else:
+             break
